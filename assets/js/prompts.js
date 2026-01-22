3a1673b9a49e042b381c75d2c4a1c6e3e09a63d1
Tarek Galal <tare2.galal@gmail.com>
2026-01-22 12:35:48 +0100

    Add query string from selected model in lib

--- assets/js/prompts.js
+++ assets/js/prompts.js
@@ -1,7 +1,7 @@
 // Prompts page JavaScript
 // Note: currentProvider and currentModel are defined in prompts.html inline script
 
-function updateProvider() {
+function updateProvider(skipQueryUpdate = false) {
     const provider = document.getElementById('providerSelect').value;
     currentProvider = provider;
 
@@ -37,9 +37,13 @@ function updateProvider() {
     }
 
     renderAllPrompts();
+
+    if (!skipQueryUpdate) {
+        updateQueryString();
+    }
 }
 
-function updateModel() {
+function updateModel(skipQueryUpdate = false) {
     const modelSelect = document.getElementById('modelSelect');
     const modelInput = document.getElementById('modelInput');
 
@@ -52,6 +56,10 @@ function updateModel() {
     }
 
     renderAllPrompts();
+
+    if (!skipQueryUpdate) {
+        updateQueryString();
+    }
 }
 
 function getModelString(provider, model) {
@@ -59,5 +67,84 @@ function getModelString(provider, model) {
       return provider;
     } else {
         return `${provider}/${model}`;
+    }
+}
+
+function updateQueryString() {
+    const params = new URLSearchParams(window.location.search);
+
+    if (currentProvider && currentProvider !== 'none') {
+        params.set('provider', currentProvider);
+    } else {
+        params.delete('provider');
+    }
+
+    if (currentModel) {
+        // Extract just the model part without provider prefix for query string
+        let modelForQuery = currentModel;
+        if (currentProvider !== 'other' && currentModel.includes('/')) {
+            // Remove "provider/" prefix from model
+            modelForQuery = currentModel.split('/')[1];
+        } else if (currentModel === currentProvider) {
+            // Model is just provider name, don't add to query string
+            modelForQuery = null;
+        }
+
+        if (modelForQuery) {
+            params.set('model', modelForQuery);
+        } else {
+            params.delete('model');
+        }
+    } else {
+        params.delete('model');
+    }
+
+    const newUrl = params.toString()
+        ? `${window.location.pathname}?${params.toString()}${window.location.hash}`
+        : `${window.location.pathname}${window.location.hash}`;
+
+    history.replaceState(null, '', newUrl);
+}
+
+function applyQueryParams() {
+    const params = new URLSearchParams(window.location.search);
+    const provider = params.get('provider');
+    const model = params.get('model');
+
+    if (provider) {
+        const providerSelect = document.getElementById('providerSelect');
+        providerSelect.value = provider;
+        currentProvider = provider;
+        updateProvider(true); // skipQueryUpdate = true to avoid circular update
+
+        if (model) {
+            if (provider === 'other') {
+                const modelInput = document.getElementById('modelInput');
+                modelInput.value = model;
+                currentModel = model;
+            } else {
+                const modelSelect = document.getElementById('modelSelect');
+                // Parse model - it might be "provider/model" or just "model"
+                let modelValue = model;
+                if (model.includes('/')) {
+                    // Extract model part from "provider/model" format
+                    modelValue = model.split('/')[1];
+                } else if (model === provider) {
+                    // Model is just the provider name, meaning "none" was selected
+                    modelValue = 'none';
+                }
+
+                // Try to set the model value
+                modelSelect.value = modelValue;
+                if (modelSelect.value === modelValue) {
+                    // Model exists in dropdown
+                    updateModel(true);
+                } else {
+                    // Model doesn't exist in dropdown, set currentModel directly
+                    currentModel = model;
+                }
+            }
+            renderAllPrompts();
+        }
     }
 }
